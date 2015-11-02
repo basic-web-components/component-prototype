89e5ecfa87655399a387fb8c0a5c20f9f115eb2f
Jan Miksovsky <jan@quickui.org>
2015-11-02 14:01:24 -0800

    Scaffolding to run Babel.

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function (grunt) {
+
+  'use strict';
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Project configuration.
+  grunt.initConfig({
+
+    browserify: {
+      options: {
+        browserifyOptions: {
+          debug: true
+        },
+        transform: ['babelify']
+      },
+      dist: {
+        files: {
+          'dist/basic-web-components.js': 'src/**/*.js'
+        }
+      }
+      // demos: {
+      //   files: {
+      //     'build/demos.js': ['demos/*.js', 'demos/**/*.js']
+      //   }
+      // },
+      // test: {
+      //   files: {
+      //     'build/tests.js': 'test/*.tests.js'
+      //   }
+      // }
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          // 'demos/*.js',
+          // 'demos/**/*.js',
+          // 'extensible/*.js',
+          'src/**/*.js'
+          // 'test/*.js'
+        ],
+        tasks: ['build']
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('build', ['browserify']);
+
+};
