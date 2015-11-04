4e4ed60554f317f72a305d4e107e2fdf7738dc79
Jan Miksovsky <jan@quickui.org>
2015-11-03 16:34:58 -0800

    Temporarily remove use of "..." spread operator.

--- components/ListBox/Super.js
+++ components/ListBox/Super.js
@@ -6,9 +6,12 @@
 
 export default class Super {
 
-  superCall(prototype, method, ...args) {
+  // As of 11/3/2015, Safari 9 doesn't yet support ES6 "..." spread operator.
+  // superCall(prototype, method, ...args) {
+  superCall(prototype, method) {
     let base = prototype.super[method];
     if (base) {
+      let args = [].slice.call(arguments, 2); // For Safari 9
       return base.apply(this, args);
     }
   }
